8da795595128d8b263302b7adfefe054e97ee55a
Swati Mishra <111337189+swati28mishra@users.noreply.github.com>
2022-08-16 12:37:30 +0530

    Update streamlit_app.py
    added breakfast favorites header with icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My parents new healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-range Egg')
+streamlit.text('🥑🍞 Avocado toast')
