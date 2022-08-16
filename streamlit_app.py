9f52d1e8191161ad7628e5a93f95a5dde765def9
Swati Mishra <111337189+swati28mishra@users.noreply.github.com>
2022-08-16 12:14:38 +0530

    Update streamlit_app.py
    Added extra content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-range Egg')
